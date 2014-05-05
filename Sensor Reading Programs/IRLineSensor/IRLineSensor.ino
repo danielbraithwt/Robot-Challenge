17b71d3d6a24f290a8fe89b3e19fddaa02e0479a
Daniel Braithwaite <danielbraithwt@gmail.com>
2014-05-05 12:55:59 +1200

    Worked on the line folowing program, still dosnt work

--- Sensor Reading Programs/IRLineSensor/IRLineSensor.ino
+++ Sensor Reading Programs/IRLineSensor/IRLineSensor.ino
@@ -1,45 +1,57 @@
 #include <QTRSensors.h>
 
 // Number of sensors
-int numOfSensors = 9;
+int numOfSensors = 8;
 // Sensor Pins ( Digital Pins )
 int pins[] = { 13, 12, 11, 10, 9, 8, 7, 6, 5 };
 // Create array for output from sensors
-unsigned int output[9];
+unsigned int output[8];
 
 // Create the sensor object
-QTRSensorsRC qtr((unsigned char[]) {2,4,5,6,7,8,9,10}, numOfSensors);
+QTRSensorsRC qtr((unsigned char[]) { 2, 4, 5, 6, 7 ,8 ,9, 10 }, numOfSensors);
 
 void setup()
 {
   Serial.begin(9600);
   
-  Serial.println("Starting callabration in 3 seconds");
-  delay(3000);
+  //Serial.println("Starting callabration in 3 seconds");
+  //delay(3000);
   
   // Calabrate the sensors
-  Serial.println("Callabrating.... Expose all your sensors to the diffrent extremes");
+  //Serial.println("Callabrating.... Expose all your sensors to the diffrent extremes");
   
-  for( int i = 0; i < i < 250; i++ )
-  {
-    qtr.calibrate();
-    delay(20);
-  }
+  //for( int i = 0; i < i < 250; i++ )
+  //{
+  //  qtr.calibrate();
+  //  delay(20);
+  //}
   
-  Serial.println("Callbration finished");
+  //Serial.println("Callbration finished");
   
 }
 
 void loop()
 {
+  qtr.read(output);
+  
+  for( int i = 0; i < 8; i++ )
+  {
+   Serial.print(output[i]);
+   Serial.print(" ");
+   
+  }
+  
+  Serial.println();
+  
   // Get the position of the line
-  int pos = qtr.readLine(output, 1);
+  //int pos = qtr.readLine(output, 1);
+  
   
   // Calculate the error
-  int error = pos - 5000;
+  //int error = pos - 5000;
   
-  Serial.print("Error: ");
-  Serial.println(error);
+  //Serial.print("Error: ");
+  //Serial.println(error);
   
   delay(100);
 }
--- Software Protope 1/Line_Folower/Line_Folower.ino
+++ Software Protope 1/Line_Folower/Line_Folower.ino
@@ -6,15 +6,19 @@ int dir_a = 12;  //dir control for motor outputs 1 and 2 is on digital pin 12
 int dir_b = 13;  //dir control for motor outputs 3 and 4 is on digital pin 13
 int move_pulse = 100;
 
+int lastError = 0;
+float KP = 0.2;
+float KD = 1;
+
 // Number of sensors
-int numOfSensors = 9;
+int numOfSensors = 8;
 // Sensor Pins ( Digital Pins )
 int pins[] = { 13, 12, 11, 10, 9, 8, 7, 6, 5 };
 // Create array for output from sensors
-unsigned int output[9];
+unsigned int output[8];
 
 // Create the sensor object
-QTRSensorsRC qtr((unsigned char[]) { 13, 12, 11, 10, 9, 8, 7, 6, 5 }, numOfSensors);
+QTRSensorsRC qtr((unsigned char[]) { 2, 4, 5, 6, 7 ,8 ,9, 10 }, numOfSensors);
 
 void setup()
 {
@@ -29,30 +33,48 @@ void setup()
   analogWrite(pwm_b, 0);
   Serial.begin(9600);     // for communacations with computer
   
-  Serial.println("Starting in 3 seconds");
-  delay(3000);
+  //Serial.println("Starting in 3 seconds");
+  //delay(3000);
   
 }
 
 void loop()
 {
   // Get the position of the line
-  qtr.readLine(output);
+  qtr.read(output);
   
-  int normOutput[9];
+  for( int i = 0; i < numOfSensors; i++ )
+  {
+   Serial.print(output[i]);
+   Serial.print(" ");
+   
+  }
+  
+  Serial.println();
+  
+  int normOutput[numOfSensors];
   
   // Normalise the output into 1 ( white ) and 0 ( black )
-  for( int i = 0; i < sizeof(output); i++ )
+  for( int i = 0; i < numOfSensors; i++ )
   {
-    if( output[i] > 550 ) normOutput[i] = 0;
+    if( output[i] > 2000 ) normOutput[i] = 0;
     else normOutput[i] = 1;
   }
+  
+  for( int i = 0; i < numOfSensors; i++ )
+  {
+   Serial.print(normOutput[i]);
+   Serial.print(" ");
+   
+  }
+  
+  Serial.println();
   
   // Sum up the sensor values 
   int average = 0;
   int count = 0;
   
-  for( int i = 0; i < sizeof(normOutput); i++ )
+  for( int i = 0; i < numOfSensors; i++ )
   {
     average += ( i * 1000 ) * normOutput[i];
     count += normOutput[i];
@@ -62,47 +84,61 @@ void loop()
   average = average / count;
   
   // Calculate the error, ie the diffrence between the middle and the average
-  int error = 4000 - average;
+  int error = 3500 - average;
   
   // Calculate the ammount to change direction by, devide by 100 because a
   // change in the thousands is far to big
-  int delta = abs(error/100);
+  //int delta = abs(error/10);
+  int delta = KP * (error/100) + KD * ( (error/100) - lastError );
+  lastError = error/100 ;
   
   Serial.print("[*] Error: ");
-  Serial.println(error);
+  Serial.print(error);
+  Serial.print(". ");
+  Serial.print("[*] Delta: ");
+  Serial.println(delta);
   
   // If error is less than 0 then that means that the line towards the right of the sensors
   
   int left = 100;
   int right = 100;
   
-  if( error < 0 )
-  {
-    Serial.println("[*] Turning Right");
-    right += delta;
-    left -= delta;
-    //Right(delta);
-  }
+  left += delta;
+  right -= delta;
+  
+  //if( error > 0 )
+  //{
+  //  Serial.println("[*] Turning Right");
+    //right += delta;
+    //left -= delta;
+  //  Right(delta);
+  //}
   // This means the line is to the left of the sensors
-  else if( error > 0 )
-  {
-    Serial.println("[*] Turning Left");
-    right -= delta;
-    left += delta;
-    //Left(delta);
-  }
+  //else if( error < 0 )
+  //{
+  //  Serial.println("[*] Turning Left");
+    //right -= delta;
+    //left += delta;
+  //  Left(delta);
+  //}
+  
+  Serial.print("[*] Left Speed: ");
+  Serial.print(left);
+  Serial.print(". ");
+  Serial.print("[*] Right Speed: ");
+  Serial.println(right);
   
   Left(left);
   Right(right);
   //Forward(100);
   
-  delay(50);
+  delay(400);
 }
 
 void Forward(int dt)
 {
-   digitalWrite(dir_a, HIGH);   // select motor direction
-   digitalWrite(dir_b, HIGH);   
+   digitalWrite(dir_a, LOW);   // select motor direction
+   digitalWrite(dir_b, LOW);   
    analogWrite(pwm_a, 255);     // set motor voltage to max
    analogWrite(pwm_b, 255);
    delay(dt);                   // wait
@@ -113,8 +149,8 @@ void Forward(int dt)
 
 void Back(int dt)
 {
-  digitalWrite(dir_a, LOW);   
-  digitalWrite(dir_b, LOW);   
+  digitalWrite(dir_a, HIGH);   
+  digitalWrite(dir_b, HIGH);   
   analogWrite(pwm_a, 255);
   analogWrite(pwm_b, 255);
   delay(dt);
@@ -125,8 +161,8 @@ void Back(int dt)
 
 void Left(int dt)
 {
-   digitalWrite(dir_a, HIGH);   
-   digitalWrite(dir_b, LOW);   
+   digitalWrite(dir_a, LOW);   
+   digitalWrite(dir_b, HIGH);   
    analogWrite(pwm_a, 255);
    analogWrite(pwm_b, 255);
    delay(dt);
@@ -137,8 +173,8 @@ void Left(int dt)
 
 void Right(int dt)
 {
-   digitalWrite(dir_a, LOW);   
-   digitalWrite(dir_b, HIGH);   
+   digitalWrite(dir_a, HIGH);   
+   digitalWrite(dir_b, LOW);   
    analogWrite(pwm_a, 255);
    analogWrite(pwm_b, 255);
    delay(dt);
